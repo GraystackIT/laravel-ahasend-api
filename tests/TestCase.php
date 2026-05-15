bd47fd4f851c0d1e34ad312af73ba6930980037d
Parvesh <parvesh.kumar@bacancy.com>
2026-05-15 17:26:47 +0530

    Remove hardcoded false keys from test file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,10 +27,10 @@ abstract class TestCase extends OrchestraTestCase
      */
     protected function defineEnvironment($app): void
     {
-        $app['config']->set('ahasend.api_key', 'test-api-key');
-        $app['config']->set('ahasend.account_id', 'test-account-id');
-        $app['config']->set('ahasend.base_url', 'https://api.ahasend.com/v2');
-        $app['config']->set('ahasend.from.address', 'from@example.com');
+        $app['config']->set('ahasend.api_key', config('ahasend.api_key'));
+        $app['config']->set('ahasend.account_id', config('ahasend.account_id'));
+        $app['config']->set('ahasend.base_url', config('ahasend.base_url'));
+        $app['config']->set('ahasend.from.address', config('ahasend.from.address'));
         $app['config']->set('ahasend.from.name', 'Test Sender');
         $app['config']->set('ahasend.store_logs', false);
         $app['config']->set('ahasend.storage_driver', 'log');
